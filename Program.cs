83b63af2fc681789104368b7aa7b0c8966c8954b
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-11 22:14:05 +0400

    Init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using CommandApp.App;
+using CommandApp.Command;
+
+namespace FlashcardApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            ICommandCollection commands = new DefaultCommandCollection();
+            RegisterCommands(commands);
+
+            IApp app = new DefaultApp(commands);
+            app.Start();
+        }
+
+        public static void RegisterCommands(ICommandCollection commands)
+        {
+
+        }
+    }
+}
