86b3c7650a3c8a9782d983ec79392c9d50ae5205
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-14 16:51:46 +0400

    Update Stack model

---
+++ Lib/FlashcardLib/Models/Stack.cs
@@ -0,0 +1,22 @@
+using System.Data.Common;
+using FlashcardApp;
+
+namespace FlashcardLib
+{
+    public class StackModel : IModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void SetFrom(DbDataReader reader)
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("Id"));
+            Name = reader.GetString(reader.GetOrdinal("Name"));
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
