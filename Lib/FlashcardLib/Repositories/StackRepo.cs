a5cefafc146901005801557aa7d0b52b153df662
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-14 16:52:12 +0400

    Implement Stack repository

---
+++ Lib/FlashcardLib/Repositories/StackRepo.cs
@@ -0,0 +1,111 @@
+using CommandApp.Exceptions;
+using Npgsql;
+
+namespace FlashcardLib
+{
+    public static class StackRepo
+    {
+        public static StackModel CreateStack(string name)
+        {
+            if (name.Length == 0)
+            {
+                throw new BaseException("Name is empty.");
+            }
+
+            Dictionary<string, object> parameters = [];
+            parameters.Add("@Name", name);
+
+            string query = $@"INSERT INTO ""Stacks""(""Name"") VALUES (@Name) RETURNING ""Id"", ""Name"";";
+
+            StackModel stack = new();
+
+            ReaderCallback assignStack = (NpgsqlDataReader reader) =>
+            {
+                if (!reader.HasRows)
+                {
+                    throw new BaseException("Something went wrong while creating a stack");
+                }
+
+                reader.Read();
+                stack.SetFrom(reader);
+            };
+
+            SqlAccess.ExecuteReader(query, parameters, assignStack);
+
+            return stack;
+        }
+
+        public static StackModel GetStack(int id)
+        {
+            string query = @"SELECT * FROM ""Stacks"" WHERE ""Id"" = @Id LIMIT 1;";
+
+            Dictionary<string, object> parameters = [];
+            parameters.Add("@Id", id);
+
+            StackModel stack = new();
+
+            ReaderCallback assignStack = (NpgsqlDataReader reader) =>
+            {
+                if (!reader.HasRows)
+                {
+                    throw new BaseException($"Stack [{id}] doesn't exist");
+                }
+
+                reader.Read();
+                stack.SetFrom(reader);
+            };
+
+            SqlAccess.ExecuteReader(query, parameters, assignStack);
+
+            return stack;
+        }
+
+        public static List<StackModel> AllStack()
+        {
+            string query = @"SELECT * FROM ""Stacks"";";
+
+            List<StackModel> stacks = [];
+
+            ReaderCallback fillStacks = (NpgsqlDataReader reader) =>
+            {
+                if (!reader.HasRows) return;
+
+                while (reader.Read())
+                {
+                    StackModel stack = new();
+                    stack.SetFrom(reader);
+                    stacks.Add(stack);
+                }
+            };
+
+            SqlAccess.ExecuteReader(query, [], fillStacks);
+
+            return stacks;
+        }
+
+        public static StackModel EditStack(StackModel stack, string name)
+        {
+            string query = @"UPDATE ""Stacks"" SET ""Name"" = @Name WHERE ""Id""=@Id;";
+
+            Dictionary<string, object> parameters = [];
+            parameters.Add("@Id", stack.Id);
+            parameters.Add("@Name", name);
+
+            SqlAccess.ExecuteNonQuery(query, parameters);
+
+            stack.Name = name;
+
+            return stack;
+        }
+
+        public static void DeleteStack(StackModel stack)
+        {
+            string query = @"DELETE FROM ""Stacks"" WHERE ""Id"" = @Id;";
+
+            Dictionary<string, object> parameters = [];
+            parameters.Add("@Id", stack.Id);
+
+            SqlAccess.ExecuteNonQuery(query, parameters);
+        }
+    }
+}
