a2beb4fedb5b9b2f86ee688c48a8004bd71e8b1d
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-14 16:50:37 +0400

    add GetIntInput helper method

--- Lib/FlashcardLib/Helpers.cs
+++ Lib/FlashcardLib/Helpers.cs
@@ -1,4 +1,6 @@
 using System.Configuration;
+using CommandApp.Exceptions;
+using CommandApp.IO;
 
 namespace FlashcardLib
 {
@@ -7,6 +9,20 @@ namespace FlashcardLib
         public static string GetConnectionString(string db)
         {
             return ConfigurationManager.ConnectionStrings[db].ConnectionString;
+        }
+
+        public static int GetIntInput(IAppInput input, string? infoText = null)
+        {
+            string value = input.Get(infoText);
+
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch (Exception)
+            {
+                throw new BaseException($"Wrong int format `{value}`");
+            }
         }
     }
 }
