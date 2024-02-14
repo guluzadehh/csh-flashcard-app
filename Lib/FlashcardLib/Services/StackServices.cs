1ba4ae29e9e1b7af141f72ecbaa61ec109ac68b1
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-14 16:52:33 +0400

    Implement Stack services

---
+++ Lib/FlashcardLib/Services/StackServices.cs
@@ -0,0 +1,25 @@
+using CommandApp.App;
+
+namespace FlashcardLib
+{
+    public class StackServices(IApp app)
+    {
+        private IApp App { get; } = app;
+
+        public void ListStacks()
+        {
+            List<StackModel> stacks = StackRepo.AllStack();
+            foreach (StackModel stack in stacks)
+            {
+                App.Output.Write($"{stack.Id}. {stack.Name}");
+            }
+        }
+
+        public StackModel GetStack()
+        {
+            ListStacks();
+            int id = Helpers.GetIntInput(App.Input, "Enter id");
+            return StackRepo.GetStack(id);
+        }
+    }
+}
